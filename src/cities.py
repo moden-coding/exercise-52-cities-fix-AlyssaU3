d91e48a6f154699dadff52c4d78485b6de06c9eb
AlyssaU3 <alys4321c@gmail.com>
2025-10-23 12:02:35 -0400

    52 done

--- src/cities.py
+++ src/cities.py
@@ -6,7 +6,11 @@ def cities():
     cities = ["Helsinki", "Espoo", "Tampere", "Vantaa", "Oulu"]
     population = [643272, 279044, 231853, 223027, 201810]
     area = [715.48, 528.03, 689.59, 240.35, 3817.52]
-    pass
+    
+    citys=pd.DataFrame(
+    {"Population":population, "Total area":area}, index =cities
+    )
+    return citys
 
 def main():
     print(cities())
